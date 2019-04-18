86a71df2c444e76a226c757663c67431f82ac1ba
Alexsander <alexsander11115@gmail.com>
2019-04-18 11:07:34 +0300

    Add name User

--- lessons16/app.js
+++ lessons16/app.js
@@ -13,24 +13,25 @@ app.use('/js', express.static('../node_modules/jquery/dist'));
 app.use('/js', express.static('../node_modules/bootstrap/dist/js'));
 app.use('/css', express.static('../node_modules/bootstrap/dist/css'));
 
+
 app.get('/',function (req,res) {
    res.render('index');
 });
+
 app.get('/chat',function (req,res) {
    res.render('chat');
 });
 
 
-let user = 0;
-io.on('connection',function (socket) {
-    user++;
-    console.log('client has been connected  ' + user);
 
-    socket.on('input value',function (msg) {
+io.on('connection',function (socket) {
+    console.log('client has been connected');
+
+    socket.on('input value',function (msg,name) {
        // console.log(msg);
 
-        // socket.broadcast.emit('input value', msg);
-        io.sockets.emit('input value', msg);
+        // socket.broadcast.emit('input value', msg); //Отправка всем кроме самого пользователя
+        io.sockets.emit('input value', msg,name); //Отправка всем
 
     });
 
--- lessons16/public/js/main.js
+++ lessons16/public/js/main.js
@@ -2,14 +2,37 @@
 const socket = io();
 const button = document.querySelector('#button-send');
 const inputTextArea = document.querySelector('#input-box');
+const msgGroup = document.querySelector('.msg-group');
+var Name = prompt('Введите ваше имя');
+
+
 
 button.addEventListener('click',function () {
-    socket.emit('input value',inputTextArea.value); });
+    socket.emit('input value', inputTextArea.value,Name);
 
-socket.on('input value', function (msg) {
-    var p = document.createElement('p');
-    p.innerHTML = msg;
-    document.body.appendChild(p);
 });
+
+    socket.on('input value', function (msg,name) {
+        var divMain = document.createElement('div'),
+            div = document.createElement('div'),
+            h6 = document.createElement('h6'),
+            p = document.createElement('p');
+        divMain.classList.add('card');
+        div.classList.add("card-body");
+        h6.classList.add("card-subtitle");
+        h6.classList.add("mb-2");
+        h6.classList.add("text-muted");
+        h6.classList.add("text-left");
+        p.classList.add('card-text');
+        p.classList.add('float-left');
+        p.innerHTML = msg;
+        h6.innerHTML = name;
+
+        div.appendChild(h6);
+        div.appendChild(p);
+        divMain.appendChild(div);
+        msgGroup.appendChild(divMain);
+        inputTextArea.value = '';
+    });
 
 socket.emit('connection');
