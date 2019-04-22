c039e361ec0486c556aa885f727bab6cdd5bc662
Alexsander <alexsander11115@gmail.com>
2019-04-22 12:15:51 +0300

    Add index.js and add work with MongoDb

---
+++ lessons17/index.js
@@ -0,0 +1,126 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/mydb";
+const objectId = require("mongodb").ObjectID;
+const path = require('path');
+
+const port = 3000;
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.render('index');
+});
+
+app.get("/customers", function (req, res) {
+
+    MongoClient.connect(url, { "useNewUrlParser": true },function (err, db) {
+        if (err) {
+            throw err;
+        }
+        const mydb = db.db('mydb');
+        mydb.collection('customers').find({}).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.send(result);
+            db.close();
+        });
+    });
+});
+// get by id
+app.get("/customers/:id", function (req, res) {
+
+    const id = new objectId(req.params.id);
+
+    MongoClient.connect(url, { "useNewUrlParser": true },function (err, db) {
+        if (err) {
+            throw err;
+        }
+        const mydb = db.db('mydb');
+        mydb.collection('customers').findOne({ _id: id }, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.send(result);
+            db.close();
+        });
+    });
+});
+
+
+app.post('/customers', function (req, res) {
+    const customer = {
+        name: req.body.name,
+        address:req.body.address
+    };
+
+    MongoClient.connect(url, { "useNewUrlParser": true },function (err, db) {
+        if (err) {
+            throw err;
+        }
+        const mydb = db.db('mydb');
+        mydb.collection('customers').insertOne(customer, function (err, data) {
+            res.send(customer);
+            db.close();
+        });
+    });
+});
+
+app.put('/customers', function (req, res) {
+
+    const id = new objectId(req.body.id);
+
+    MongoClient.connect(url,{ "useNewUrlParser": true }, function (err, db) {
+        if (err) {
+            throw err;
+        }
+        const mydb = db.db('mydb');
+        const myquery = { _id: id };
+        const newValues = {
+            $set: {
+                name: req.body.name,
+                address:req.body.address
+            }
+        };
+        mydb.collection('customers').updateOne(myquery, newValues, function(err, result) {
+                if (err) throw err;
+                console.log("1 document updated");
+                res.send(result);
+                db.close();
+            });
+    });
+});
+
+
+
+
+// Delete
+app.delete("/customers/:id", function (req, res) {
+    const id = new objectId(req.params.id);
+
+    MongoClient.connect(url, { "useNewUrlParser": true },function (err, db) {
+        if (err) {
+            throw err;
+        }
+        const mydb = db.db('mydb');
+
+        const myquery = { _id: id };
+        mydb.collection('customers').deleteOne(myquery, function(err, obj) {
+                if (err) throw err;
+                console.log("1 document deleted");
+                db.close();
+            });
+    });
+});
+
+
+http.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
--- lessons17/main.js
+++ lessons17/main.js
@@ -4,11 +4,71 @@ const url = "mongodb://localhost:27017/mydb";
 MongoClient.connect(url, { "useNewUrlParser": true },function(err, db) {
     if (err) throw err;
     console.log("Database created!");
-    var dbo = db.db("mydb");
+
+    const dbo = db.db("mydb");
+
+    let customer = { name: "Alexsander", address: "Highway 37" };
+
+    var myquery = { address: "Canyon 123" };
+    var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
+
+    var myobj = [
+        { name: 'John', address: 'Highway 71'},
+        { name: 'Peter', address: 'Lowstreet 4'},
+        { name: 'Amy', address: 'Apple st 652'},
+        { name: 'Hannah', address: 'Mountain 21'},
+        { name: 'Michael', address: 'Valley 345'},
+        { name: 'Sandy', address: 'Ocean blvd 2'},
+        { name: 'Betty', address: 'Green Grass 1'},
+        { name: 'Richard', address: 'Sky st 331'},
+        { name: 'Susan', address: 'One way 98'},
+        { name: 'Vicky', address: 'Yellow Garden 2'},
+        { name: 'Ben', address: 'Park Lane 38'},
+        { name: 'William', address: 'Central st 954'},
+        { name: 'Chuck', address: 'Main Road 989'},
+        { name: 'Viola', address: 'Sideway 1633'}
+    ];
 
     dbo.createCollection("customers", function(err, res) {
         if (err) throw err;
         console.log("Collection created!");
-        db.close();
+
+
+        //Find the first document in the customers collection:
+        dbo.collection("customers").findOne({address:"Highway 71"}, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
+
+        //Insert a document in the "customers" collection:
+        // dbo.collection("customers").insertOne(customer, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 document inserted");
+        //     db.close();
+        // });
+
+        //Insert multiple documents in the "customers" collection:
+        // dbo.collection("customers").insertMany(myobj, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("Number of documents inserted: " + res.insertedCount);
+        //     db.close();
+        // });
+
+        //If the query finds more than one record, only the first occurrence is updated.
+        //Update the document with the address "Highway 37" to name="Mickey" and address="Canyon 123":
+
+        // dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 document updated");
+        //     db.close();
+        // });
+
+        //If the query finds more than one document, only the first occurrence is deleted.
+        // dbo.collection("customers").deleteOne(myquery, function(err, obj) {
+        //     if (err) throw err;
+        //     console.log("1 document deleted");
+        //     db.close();
+        // });
     });
 });
